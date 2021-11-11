2c455cd788ea43096d35cfdb62c16119b03e8bf2
shakil89427 <shakilahmed89427@gmail.com>
2021-11-11 23:51:32 +0600

    review added

--- index.js
+++ index.js
@@ -62,6 +62,87 @@ async function run() {
   } finally {
     await client.close();
   };
+  /* Add To Cart */
+  try {
+    app.post("/addtocart", async (req, res) => {
+      await client.connect();
+      const data = req.body
+      const database = client.db("cart");
+      const cart = database.collection(data.userId);
+      const findby = {_id:data._id};
+      const result = await cart.findOne(findby);
+      if(!result){
+        data.quantity = 1
+        const again = await cart.insertOne(data)
+        res.send(again)
+      }
+      else{
+        const newquantity = result.quantity+1
+        const updated = {
+          $set:{quantity: newquantity}
+        }
+        const update = await cart.updateOne(findby,updated)
+        res.send(update)
+      }
+    });
+  } finally {
+    await client.close();
+  };
+  /* Get All from cart */
+  try {
+    app.get("/allcart/:id", async (req, res) => {
+      await client.connect();
+      const userId = req.params.id
+      const database = client.db("cart");
+      const cart = database.collection(userId);
+      const allitems = cart.find({});
+      const result = await allitems.toArray();
+      res.send(result)
+    });
+  } finally {
+    await client.close();
+  };
+  /* Delate from cart */
+  try {
+    app.delete("/deletefromcart/:id", async (req, res) => {
+      await client.connect();
+      const userId = req.query.uid
+      const id = req.params.id
+      const database = client.db("cart");
+      const cart = database.collection(userId);
+      const item = {_id:id};
+      const result = await cart.deleteOne(item)
+      res.send(result)
+    });
+  } finally {
+    await client.close();
+  };
+  /* Add Review */
+  try {
+    app.post("/addreview", async (req, res) => {
+      await client.connect();
+      const data = req.body
+      const database = client.db("reviews");
+      const reviews = database.collection('allreviews');
+      const result = await reviews.insertOne(data)
+      res.send(result)
+    });
+  } finally {
+    await client.close();
+  };
+  /* Get All Reviews */
+  try {
+    app.get("/allreviews", async (req, res) => {
+      await client.connect();
+      const database = client.db("reviews");
+      const reviews = database.collection('allreviews');
+      const allitems = reviews.find({});
+      const result = await allitems.toArray();
+      res.send(result)
+    });
+  } finally {
+    await client.close();
+  };
 }
 run().catch(console.dir);
 
